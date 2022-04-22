5faccea4699c1e3c4abcc3897edb89080371b9c5
Булат Суфиянов <sufiyanov.b@mail.ru>
2022-04-22 12:22:57 +0300

    change readme

--- ValidationAttributes/Program.cs
+++ ValidationAttributes/Program.cs
@@ -6,6 +6,23 @@ namespace ValidationAttributes
     {
         static void Main(string[] args)
         {
+
+
+            //var obj = new ValidationAttributesTest() { Inn = 16500891007305, NumCard = 4276380038769571 };
+
+            //var results = new List<ValidationResult>();
+
+            //var context = new ValidationContext(obj);
+
+            //if (!Validator.TryValidateObject(obj, context, results, true))
+            //{
+            //    foreach (var error in results)
+            //    {
+            //        Console.WriteLine(error.ErrorMessage);
+            //    }
+            //}
+
+
             Console.WriteLine("Hello World!");
         }
     }
